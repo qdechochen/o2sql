84bcabbb0f62b960614f0a8ab951156f3de46215
Echo Chen <echo.chen@EchodeMacBook-Pro.local>
2018-07-23 15:58:39 +0800

    executehandler ok

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const { Command } = require('./lib/command');
+const { setOnExecuteHandler } = require('./lib/command');
 const Osql = require('./lib/osql');
 const parse = require('./lib/pegjs-parser').parse;
 
 const osql = options => Osql(options);
 osql.parse = parse;
-osql.setExecuteHandler = (cb) => {
-  Command.prototype.onExecuteHandler = cb;
-};
+osql.setOnExecuteHandler = cb => setOnExecuteHandler(cb);
 
 module.exports = osql;
