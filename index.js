da0632187601c85c8f04300af37a487487f9e100
Echo Chen <echo.chen@EchodeMacBook-Pro.local>
2018-07-23 12:17:42 +0800

    增加 executeHandler 支持

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const { Command } = require('./command');
 const Osql = require('./lib/osql');
 const parse = require('./lib/pegjs-parser').parse;
 
 const osql = options => Osql(options);
 osql.parse = parse;
+osql.setExecuteHandler = (cb) => {
+  Command.onExecute(cb);
+};
 
 module.exports = osql;
