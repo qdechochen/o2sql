7404495b79f0147db7a59471e8e6d50621ce2f74
Echo Chen <echo.chen@EchodeMacBook-Pro.local>
2018-10-11 14:49:16 +0800

    修复表join时，相关的主外键所属表名处理错误的问题

--- test/index.js
+++ test/index.js
@@ -114,3 +114,40 @@ console.log(o2sql.insertInto('project')
     }),
     likes: null,
   }]).toParams());
+
+
+console.dir(o2sql('select').from({
+  left: {
+    left: {
+      name: 'user',
+      alias: 'U',
+      key: 'classId',
+    },
+    right: {
+      name: 'class',
+      alias: 'C',
+      key: 'id',
+    },
+    key: 'U.gradeId',
+  },
+  right: {
+    name: 'grade',
+    alias: 'G',
+  },
+}).columns([{
+  table: 'U', prefix: 'user', separator: '_', fields: ['id', 'name']
+}, 'id', 'name', ['class', 'className'], [o2sql.parse('convert(a, 101)'), 'dt']])
+  .where({
+    a: 1,
+    b: 2,
+    e: 'abcd',
+    $or: { c: 3, d: 4 }
+  })
+  .groupby(['a.id', 'b.id'])
+  .orderby(['a.id', '-b.id', ['c.id', 'desc']])
+  .limit(4)
+  .skip(8)
+  .toParams());
+
+
+console.dir(o2sql('select').from({ left: { name: 'user', alias: 'U' }, right: 'company' }).toParams());
