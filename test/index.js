61394896013b5e13903e43e2f375083b7d224ea6
Echo Chen <echo.chen@umoregroup.com>
2018-12-28 21:46:43 +0800

    add group support for columns

--- test/index.js
+++ test/index.js
@@ -1,18 +1,37 @@
 const o2sql = require('../index');
 
-console.log(o2sql.update('user')
-  .set({
-    name: 'Echo',
-    age: 34,
-    count: o2sql.parse('"count" + 1'),
-    likes: o2sql.count('ul').where({
-      tt: 3,
-    })
-  })
-  .where({
-    $or: {
-      id: 1,
-      name: '3',
+const o = o2sql('select').from({
+  left: {
+    left: {
+      name: 'user',
+      alias: 'U',
+      key: 'classId',
     },
-    id: 1,
-  }).toParams());
+    right: {
+      name: 'class',
+      alias: 'C',
+      key: 'id',
+      main: true,
+    },
+    key: 'U.gradeId',
+  },
+  right: {
+    name: 'grade',
+    alias: 'G',
+  },
+}).columns([{
+  table: 'U', prefix: 'user', separator: '_', fields: ['id', 'name']
+}, 'id', 'name', ['class', 'className'], [o2sql.parse('convert(a, 101)'), 'dt']])
+  .where({
+    'U.a': 1,
+    b: 2,
+    e: 'abcd',
+    $or: { c: 3, d: 4 }
+  })
+  .groupby(['a.id', 'b.id'])
+  .orderby(['a.id', '-b.id', ['c.id', 'desc']])
+  .limit(4)
+  .skip(8);
+
+console.log(JSON.stringify(o.ast, 2, 2));
+console.dir(o.toParams());
