aa3343625d5542116632b34ac8a3825cf1f8a4bb
Echo Chen <echo.chen@umoregroup.com>
2019-06-13 13:54:27 +0800

    fixed bug: mainTable not resolved in count()

--- test/index.js
+++ test/index.js
@@ -1,10 +1,17 @@
 const o2sql = require('../index');
 
-const q = o2sql.select(['keyword', [o2sql.parse('count(id)'), 'count']]).from('keyword').where({
-  d: {
-    BETWEEN: [2, 3]
+const q = o2sql.select(
+  ['id']).from({
+  left: {
+    name: 'talk',
+    key: 'companyId',
+    main: true,
   },
-  e: [4, 5]
-}).groupby(['keyword'])
-  .orderby(['-count']);
+  right: {
+    name: 'company',
+    key: 'id',
+  },
+}).where({
+  status: 'pnd',
+});
 console.dir(q.toParams());
