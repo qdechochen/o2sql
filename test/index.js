28891b61d6f388425b4dbdf29e94c5b080130bbc
Echo Chen <echo.chen@umoregroup.com>
2019-09-27 13:42:51 +0800

    v2.0.7

--- test/index.js
+++ test/index.js
@@ -1,11 +1,20 @@
 const o2sql = require('../index');
 
-const q = o2sql
-.update('company')
-.set({
-  techCount: o2sql.count('tech').where({ companyId: 1 }),
-})
-.where(1)
-
+const q = o2sql.count({
+  name: o2sql
+    .select([
+      [o2sql.f('to_char', o2sql.i('createdAt'), 'yyyy-mm-dd'), 'd'],
+      'type',
+      'userId',
+      'targetId',
+    ])
+    .from('viewLog')
+    .distinct()
+    .where({
+      createdAt: {
+        BETWEEN: [111, 222],
+      },
+    }),
+});
 console.dir(q.ast);
 console.dir(q.toParams());
