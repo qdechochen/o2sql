b7ca5be07e4a45afe62576ede26fb640243271c7
Echo Chen <echo.chen@umoregroup.com>
2019-10-21 16:51:04 +0800

    v3.0.0

--- test/index.js
+++ test/index.js
@@ -1,5 +1,8 @@
 const o2sql = require('../lib/o2sql');
 
-let x = o2sql.insert({ a: 1, b: 2 }).into('user');
+let x = o2sql
+  .insert({ a: 1, b: 2 })
+  .into('user')
+  .returning(['x', 'y']);
 console.dir(x, { depth: 8 });
 console.dir(x.toParams(), { depth: 8 });
