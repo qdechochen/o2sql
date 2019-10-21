83c51ea5ea87ec30d746296b3a772c0999124137
Echo Chen <echo.chen@umoregroup.com>
2019-10-21 15:53:56 +0800

    removed

--- index.js
+++
@@ -1,6 +0,0 @@
-const o2ast = require('./lib/o2ast');
-
-const o2sqlFactory = opts => {};
-
-o2sqlFactory.function = (name, ...args) => o2ast.function(name, ...args);
-o2sqlFactory.f = o2sqlFactory.function;
