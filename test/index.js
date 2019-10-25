1b6b38974d9acd726d197106a159e132dc7cf6e8
Echo Chen <echo.chen@umoregroup.com>
2019-10-25 13:39:01 +0800

    some bug fixes

--- test/index.js
+++ test/index.js
@@ -1,8 +1,31 @@
 const o2sql = require('../lib/o2sql');
 
 let x = o2sql
-  .insert({ a: 1, b: 2 })
-  .into('user')
-  .returning(['x', 'y']);
+  .select(['id'])
+  .from('a')
+  .where({
+    a: {
+      '>': 3,
+      '<': 5,
+      '<>': 4,
+    },
+    $1: [
+      {
+        b: {
+          '>': 3,
+          '<': 5,
+          '<>': 4,
+        },
+      },
+      {
+        c: {
+          '>': 3,
+          '<': 5,
+          '<>': 4,
+        },
+      },
+    ],
+  });
+
 console.dir(x, { depth: 8 });
 console.dir(x.toParams(), { depth: 8 });
