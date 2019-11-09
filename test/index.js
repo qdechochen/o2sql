5deea5f6a56c9fc341fc8c93f8556fb91263627d
Echo Chen <echo.chen@umoregroup.com>
2019-11-09 10:36:41 +0800

    v3.0.2 added: returning support to _delete_

--- test/index.js
+++ test/index.js
@@ -1,31 +1,9 @@
 const o2sql = require('../lib/o2sql');
 
 let x = o2sql
-  .select(['id'])
-  .from('a')
-  .where({
-    a: {
-      '>': 3,
-      '<': 5,
-      '<>': 4,
-    },
-    $1: [
-      {
-        b: {
-          '>': 3,
-          '<': 5,
-          '<>': 4,
-        },
-      },
-      {
-        c: {
-          '>': 3,
-          '<': 5,
-          '<>': 4,
-        },
-      },
-    ],
-  });
+  .delete('a')
+  .where(2)
+  .returning(['x', 'y']);
 
 console.dir(x, { depth: 8 });
 console.dir(x.toParams(), { depth: 8 });
